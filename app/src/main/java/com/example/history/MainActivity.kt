0c3bdd4de0ece1ef81dc209c2806c2dfa724047b
HYEWON PARK <hyewon9913@gmail.com>
2022-01-11 03:00:35 +0900

    splash,bottomNavigation

--- app/src/main/java/com/example/history/MainActivity.kt
+++ app/src/main/java/com/example/history/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.history
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.history.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,BottomNavigationView.OnNavigationItemSelectedListener{
+    private var mBinding:ActivityMainBinding?=null
+    private val binding get() = mBinding!!
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bnvMain.setOnNavigationItemSelectedListener(this)
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_writing->{
+                var writeFragment = WriteFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container,writeFragment).commit()
+                return true
+            }
+            R.id.action_test->{
+                var testFragment = TestFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container,testFragment).commit()
+                return true
+            }
+            R.id.action_search->{
+                var searchFragment = SearchFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container,searchFragment).commit()
+                return true
+            }
+            R.id.action_myPage->{
+                var myPageFragment = MyPageFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container,myPageFragment).commit()
+                return true
+            }
+
+
+        }
+        return false
+
+    }
+
 }
