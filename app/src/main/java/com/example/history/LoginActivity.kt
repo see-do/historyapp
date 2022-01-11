d2931ff9e633aab795c08dd85dcd8491247099dd
duck <gyeondeo@gmail.com>
2022-01-11 20:09:31 +0900

    add LoginActivity, SignUpActivity

--- app/src/main/java/com/example/history/LoginActivity.kt
+++ app/src/main/java/com/example/history/LoginActivity.kt
@@ -1,4 +1,20 @@
 package com.example.history
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.history.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginEnterBtn.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+    }
 }
