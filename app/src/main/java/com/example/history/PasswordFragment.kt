7fd4f79c0e5fc75c7df2899c7249cf8c8e356888
duck <gyeondeo@gmail.com>
2022-01-12 17:17:08 +0900

    SignUpActivity fragment 전환 추가

---
+++ app/src/main/java/com/example/history/PasswordFragment.kt
@@ -0,0 +1,19 @@
+package com.example.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_password, container, false)
+    }
+}
--- app/src/main/java/com/example/history/SignUpActivity.kt
+++ app/src/main/java/com/example/history/SignUpActivity.kt
@@ -1,6 +1,46 @@
 package com.example.history
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.history.databinding.ActivitySignupBinding
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignupBinding
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val transaction = supportFragmentManager.beginTransaction()
+            .replace(R.id.signup_frm, NicknameFragment())
+        transaction.commit()
+
+        binding.signupNextBtn.setOnClickListener{
+            changeFragment()
+        }
+    }
+    private fun changeFragment(){
+        when (count) {
+            0 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.signup_frm, IdFragment())
+                    .commit()
+                count++
+            }
+            1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.signup_frm, PasswordFragment())
+                    .commit()
+                count++
+            }
+            2 -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                count = 0
+            }
+        }
+    }
 }
