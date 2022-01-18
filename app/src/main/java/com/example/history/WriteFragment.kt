df15898c681f95ed418e226b988065460a76807f
duck <gyeondeo@gmail.com>
2022-01-18 19:26:15 +0900

    writeFragment

--- app/src/main/java/com/example/history/WriteFragment.kt
+++ app/src/main/java/com/example/history/WriteFragment.kt
@@ -1,17 +1,23 @@
 package com.example.history
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.history.databinding.FragmentWriteBinding
 
 class WriteFragment : Fragment() {
+    lateinit var binding : FragmentWriteBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentWriteBinding.inflate(inflater, container, false)
+
+
         return inflater.inflate(R.layout.fragment_write,container,false)
     }
 }
