f51c0414503bf1ccaf1627e1324c5f58ecd650bc
HYEWON PARK <hyewon9913@gmail.com>
2022-02-13 03:11:07 +0900

    Lock screen

--- app/src/main/java/com/example/history/LockActivity.kt
+++ app/src/main/java/com/example/history/LockActivity.kt
@@ -2,6 +2,7 @@ package com.example.history
 
 import android.os.Build
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.history.databinding.ActivityLockBinding
@@ -12,28 +13,25 @@ class LockActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        showOnLockScreenAndTurnScreenOn()
 
         mBinding = ActivityLockBinding.inflate(layoutInflater)
         setContentView(binding.root)
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED)
         window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD)
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
-
+//        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+//        window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON)
 
 
     }
-    private fun showOnLockScreenAndTurnScreenOn() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            setShowWhenLocked(true)
-            setTurnScreenOn(true)
-        } else {
-            window.addFlags(
-                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                        or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-            )
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                finish()
+                true
+            }
+            else -> super.onTouchEvent(event)
         }
     }
+
 
 }
