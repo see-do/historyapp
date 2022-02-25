3a612062e6ec56f8a7bd1fb437ed175204ec5c80
duck <gyeondeo@gmail.com>
2022-02-25 23:57:45 +0900

    add getTest function

--- app/src/main/java/com/umc/history/TestResponse.kt
+++ app/src/main/java/com/umc/history/TestResponse.kt
@@ -1,16 +1,20 @@
 package com.umc.history
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
-
+@Parcelize
 data class GetTestResponse(
     @SerializedName("status") val status : String,
     @SerializedName("body") val body : List<Test>?
-)
+    ) : Parcelable
+
+@Parcelize
 data class Test(
     @SerializedName("quizIdx") val quizIdx : Int,
     @SerializedName("category") val category : String,
     @SerializedName("question") val question : String,
     @SerializedName("answer") val answer : Boolean,
     @SerializedName("solution") val solution : String
-)
+) : Parcelable
